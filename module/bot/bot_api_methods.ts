03e253441b0a0d9b1aea1373de2e59c3e2d3e006
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-05-19 19:28:28 +0300

    renamed

---
+++ module/bot/bot_api_methods.ts
@@ -0,0 +1,20 @@
+import type {
+  IUser,
+  IGetUpdates,
+  IUpdate,
+  ISendMessage,
+  IMessage,
+} from "./bot_api_types.ts";
+import { setUrl } from "./helpers.ts";
+import httpClient from "./httpClient.ts";
+
+export const getMe = async () => await httpClient.get<IUser>(setUrl("getMe"));
+
+export const getUpdates = async (query?: IGetUpdates) => {
+  return query
+    ? await httpClient.get<IUpdate>(setUrl("getUpdates", query))
+    : await httpClient.get<IUpdate>(setUrl("getUpdates"));
+};
+
+export const sendMessage = async (query: ISendMessage) =>
+  await httpClient.get<IMessage>(setUrl("sendMessage", query));
