948803ae023a37a9a3b1405482e68d8a6f265a67
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-06-09 15:43:11 +0300

    new table

--- module/bot/databaseTables.ts
+++ module/bot/databaseTables.ts
@@ -1,8 +1,6 @@
 import db from "./database.ts";
 
-/**
- * List of all exist tables
- */
+/** List of all exist tables */
 const tables = {
   bot: "bot",
   users: "users",
@@ -11,6 +9,7 @@ const tables = {
   medicine: "medicine",
   road_signs: "road_signs",
   road_marking: "road_marking",
+  inscriptions_and_symbols: "inscriptions_and_symbols",
 };
 
 export type TTableList = keyof typeof tables;
@@ -19,9 +18,7 @@ export default tables;
 
 /* CREATE TABLE IF NOT EXIST */
 
-/**
- * Table only for bot
- */
+/** Table only for bot */
 export const botColumns = {
   id: "INTEGER PRIMARY KEY AUTOINCREMENT",
   offset: "TEXT NOT NULL",
@@ -29,9 +26,7 @@ export const botColumns = {
 export type TBotColumns = keyof typeof botColumns;
 db.createTable(tables.bot, botColumns);
 
-/**
- * Table only for users data
- */
+/** Table only for users data */
 export const usersColumns = {
   id: "INTEGER PRIMARY KEY",
   is_bot: "INTEGER NOT NULL",
@@ -49,13 +44,11 @@ export type TUserRow = [
   first_name: string,
   last_name?: string,
   username?: string,
-  language_code?: string,
+  language_code?: string
 ];
 db.createTable(tables.users, usersColumns);
 
-/**
- * Table only for traffic rules
- */
+/** Table only for traffic rules */
 export const trafficRulesColumns = {
   id: "INTEGER PRIMARY KEY AUTOINCREMENT",
   header: "TEXT NOT NULL",
@@ -72,42 +65,58 @@ export type TTrafficRuleRow = [
   id_0: number,
   header_1: string,
   text_2: string,
-  updated_day_3: number,
+  updated_day_3: number
 ];
 db.createTable(tables.traffic_rules, trafficRulesColumns);
 
-/**
- * Table for tests
- */
+/** Table for tests "tests_pdr" */
 export interface ITestPdr {
   id: number;
   qustion: string;
   answer: string;
-  // images: string[] | null;
   updated_day: number | null;
+  theme: string | null;
+  photo_name: string | null;
+  item: number | null;
+  file_id: string | null;
 }
 
 export type TTestPdrRow = [
   id_0: number,
   qustion_1: string,
   answer_2: string,
-  // images_4: string, // as json in string type
   updated_day_3: number,
+  theme_4: string | null,
+  photo_name_5: string | null,
+  item_6: number | null,
+  file_id_7: string | null
+];
+
+export const testKeys = [
+  "id",
+  "qustion",
+  "answer",
+  "updated_day",
+  "theme",
+  "photo_name",
+  "item",
+  "file_id",
 ];
 
 const testPdrColumns: Record<keyof ITestPdr, string> = {
   id: "INTEGER PRIMARY KEY AUTOINCREMENT",
   qustion: "TEXT NOT NULL UNIQUE",
   answer: "TEXT NOT NULL",
-  // images: "TEXT", // as json in string type
   updated_day: "INTEGER",
+  theme: "TEXT",
+  photo_name: "TEXT",
+  item: "INTEGER",
+  file_id: "TEXT",
 };
 
 db.createTable(tables.tests_pdr, testPdrColumns);
 
-/**
- * Table for medicine
- */
+/** Table for medicine */
 interface IMedicine {
   id: number;
   header: string;
@@ -124,9 +133,7 @@ const medicineColumns: Record<keyof IMedicine, string> = {
 
 db.createTable(tables.medicine, medicineColumns);
 
-/**
- * Table for road sign
- */
+/** Table for road sign */
 export interface IRoadSign {
   id: number;
   header: string;
@@ -144,7 +151,7 @@ export type TRoadSign = [
   description_3: string,
   file_id_4: string,
   photo_name_5: string,
-  updated_day_6: number,
+  updated_day_6: number
 ];
 
 export const roadKeys = [
@@ -169,9 +176,7 @@ const roadSignColumns: Record<keyof IRoadSign, string> = {
 
 db.createTable(tables.road_signs, roadSignColumns);
 
-/**
- * Table for road marking
- */
+/** Table for road marking */
 export interface IRoadMark {
   id: number;
   header: string;
@@ -189,7 +194,7 @@ export type TRoadMark = [
   description_3: string,
   file_id_4: string,
   photo_name_5: string,
-  updated_day_6: number,
+  updated_day_6: number
 ];
 
 export const roadMarkingKeys = [
@@ -207,9 +212,51 @@ const roadMarkingColumns: Record<keyof IRoadMark, string> = {
   header: "TEXT NOT NULL",
   number: "TEXT NOT NULL UNIQUE",
   description: "TEXT NOT NULL",
-  file_id: "TEXT UNIQUE",
+  file_id: "TEXT",
   photo_name: "TEXT NOT NULL",
   updated_day: "INTEGER",
 };
 
 db.createTable(tables.road_marking, roadMarkingColumns);
+
+/** Table for inscriptions and symbols */
+export interface IInscriptionsAndSymbols {
+  id: number;
+  header: string;
+  description: string;
+  photo_name: string;
+  file_id: string | null;
+  updated_day: number | null;
+}
+
+export type TInscriptionsAndSymbolsRow = [
+  id_0: number,
+  header_1: string,
+  description_2: string,
+  photo_name_3: string,
+  file_id_4: string | null,
+  updated_day_5: number | null
+];
+
+export const inscriptionsAndSymbolsKeys = [
+  "id",
+  "header",
+  "description",
+  "photo_name",
+  "file_id",
+  "updated_day",
+];
+
+const inscriptionsAndSymbolsColumns: Record<
+  keyof IInscriptionsAndSymbols,
+  string
+> = {
+  id: "INTEGER PRIMARY KEY AUTOINCREMENT",
+  header: "TEXT NOT NULL",
+  description: "TEXT NOT NULL",
+  photo_name: "TEXT NOT NULL",
+  file_id: "TEXT",
+  updated_day: "INTEGER",
+};
+
+db.createTable(tables.inscriptions_and_symbols, inscriptionsAndSymbolsColumns);
